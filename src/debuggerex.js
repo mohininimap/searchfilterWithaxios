4cceddd1022e4c507821d225a5ce6f84f65e03fb
mohininimap <mohiniraut@nimapinfotech.com>
2023-03-16 19:07:07 +0530

    debugger example

---
+++ src/debuggerex.js
@@ -0,0 +1,18 @@
+function newNumber(){
+return 55;
+}
+
+function newZValue(){
+    return 75
+}
+var no=12;
+var sum=100;
+
+no=13;
+
+let z=no+sum;
+
+no=newNumber();
+z=newZValue();
+
+console.log(no,z)
